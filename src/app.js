546affcc640f979d3b7e228fc33f3329ec98106d
Takumi MIURA <mitaku1104@gmail.com>
2016-05-29 20:03:51 +0900

    Add styles

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import styles from './styles.module.css'
+
 const App = React.createClass({
   render: function() {
-    return (<div>Text text text</div>);
+    return (
+      <div className={styles['container']}>
+        Text text text
+      </div>
+    );
   }
 });
 
