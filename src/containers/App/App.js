08fee6e7bf620af08fcf51d2a049dfa50bbeb927
Takumi MIURA <mitaku1104@gmail.com>
2016-05-30 00:37:00 +0900

    Add browserHistory

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,16 +2,23 @@ import React, { PropTypes } from 'react';
 import { Router } from 'react-router';
 
 class App extends React.Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired
+  }
+
   content() {
-    return (<Router />)
+    return (<Router
+        history={this.props.history} />)
   }
 
   render() {
     return (
       <div style={{ height: '100%' }}>
+        <h1>content</h1>
         {this.content}
       </div>
     )
   }
 }
 
+export default App;
