b18ad55484f93048ce8567e4bdd91c8fc87d971e
Takumi MIURA <mitaku1104@gmail.com>
2016-05-29 23:32:33 +0900

    Add Router

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { PropTypes } from 'react';
+import { Router } from 'react-router';
 
-import styles from './styles.module.css'
+class App extends React.Component {
+  content() {
+    return (<Router />)
+  }
 
-const App = React.createClass({
-  render: function() {
+  render() {
     return (
-      <div className={styles.wrapper}>
-        <h1>
-          <i className="fa fa-star"></i>
-          Environment: {__NODE_ENV__}</h1>
+      <div style={{ height: '100%' }}>
+        {this.content}
       </div>
     )
   }
-});
+}
 
-module.exports = App;
