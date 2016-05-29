6922e8865d6122e8093ac27ea685440462dc198f
Takumi MIURA <mitaku1104@gmail.com>
2016-05-29 20:36:02 +0900

    Global Config setting

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ const modules = join(root, 'node_modules');
 const dest    = join(root, 'dist');
 
 const NODE_ENV = process.env.NODE_ENV;
+const dotenv = require('dotenv');
 const isDev = NODE_ENV === 'development';
 
 var config = getConfig({
@@ -20,6 +21,33 @@ var config = getConfig({
   out: dest,
   clearBeforeBuild: true
 })
+
+// DotEnv
+
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+
+const envVariables =
+    Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo, key) => {
+    const val = JSON.stringify(envVariables[key]);
+    memo[`__${key.toUpperCase()}__`] = val;
+    return memo;
+  }, {
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
+  });
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
+
+// CSS
 
 config.postcss = [].concat([
   require('precss')({}),
@@ -57,5 +85,6 @@ config.module.loaders.push({
   include: [modules],
   loader: 'style!css'
 })
+
 
 module.exports = config;
