99dedafe9601abea1f8487bbfbd91cb4894bd457
Takumi MIURA <mitaku1104@gmail.com>
2016-05-29 22:02:36 +0900

    Add tests.webpack.js

--- karma.conf.js
+++ karma.conf.js
@@ -24,11 +24,13 @@ module.exports = function(config) {
       'karma-chai',
       'karma-webpack',
       'karma-phantomjs-launcher',
-      'karma-spec-reporter'
+      'karma-spec-reporter',
+      'karma-sourcemap-loader'
     ],
 
     // list of files / patterns to load in the browser
     files: [
+      'tests.webpack.js'
     ],
 
 
@@ -40,6 +42,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'tests.webpack.js': ['webpack', 'sourcemap']
     },
 
 
